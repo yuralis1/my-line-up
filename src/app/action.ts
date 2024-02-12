c6237f53864ff0a8e9d3511ee3393810d5cc4b09
Nick B <nick.broeks@live.nl>
2024-02-12 13:27:36 +0100

    minor: removed unnecessary "use server" directives

--- src/app/action.ts
+++ src/app/action.ts
@@ -4,13 +4,11 @@ import { revalidatePath } from "next/cache";
 import { caller } from "@/lib/trpc";
 
 export async function addUser() {
-  "use server";
   await caller.createUser({ name: "Alice" });
   revalidatePath("/");
 }
 
 export async function removeAllUsers() {
-  "use server";
   await caller.deleteAllUsers();
   revalidatePath("/");
 }
